51f7c6d027cb0b22769b7c1586ef1023ea4491c8
HassanAhmedKhan31 <161369304+HassanAhmedKhan31@users.noreply.github.com>
2024-05-17 10:37:27 +0500

    first commit

---
+++ area.js
@@ -0,0 +1,7 @@
+function area(radius) {
+    var pi = 3.141592;
+    var area = pi * radius * radius;
+    console.log("the area of circle is :", area);
+    return (area);
+}
+area(9);
---
+++ area.ts
@@ -0,0 +1,9 @@
+function are(radius:number){
+    let pi = 3.141592
+    let area = pi*radius*radius
+    console.log("the area of circle is :",area)
+    return(are)
+}
+are(9)
+ 
+let 
